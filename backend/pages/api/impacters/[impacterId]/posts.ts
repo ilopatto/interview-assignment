2df52230cb59ceed662a6d18190c5f52b745de88
ilopatto <ivan.lopatto@gmail.com>
2020-12-28 16:36:02 +0200

    Add [/api/impacters/:impacterId/posts] API endpoint

---
+++ backend/pages/api/impacters/[impacterId]/posts.ts
@@ -0,0 +1,56 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { pool } from '@db'
+import Joi from 'joi'
+import { PostDAL } from '@components/post'
+
+type AvailableMethodResolvers = 'GET';
+
+const idValidator = Joi.number().min(1).required();
+
+export default async function impactersHandler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const validationResult = idValidator.validate(req.query.impacterId);
+  
+  if (validationResult.error) {
+    return res.status(400).json({
+      message: 'Invalid impacter id'
+    });
+  }
+
+  const impacterId = validationResult.value;
+
+  switch (req.method as AvailableMethodResolvers) {
+    case 'GET':
+      return await get(req, res, impacterId);
+      break;
+    default:
+      res.setHeader('Allow', 'GET');
+      return res
+        .status(405)
+        .json({
+          message: 'Wrong HTTP method'
+        });
+      break;
+  }
+}
+
+async function get(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  impacterId: number
+) {
+  try {
+    const postDAL = new PostDAL(pool);
+    const posts = (await postDAL.getPostsByImpacterId(impacterId)) ?? [];
+
+    return res.status(200).json(posts);
+  } catch (err) {
+    console.error(err);
+  }
+
+  return res.status(500).json({
+    message: 'Something went wrong'
+  });
+}
